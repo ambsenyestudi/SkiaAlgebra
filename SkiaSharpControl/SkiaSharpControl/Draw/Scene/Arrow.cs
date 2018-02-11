93806a1139ed6f42ed794e1747ac336a55d4bd27
Adrià Freixas <adrifre7@hotmail.com>
2018-02-11 21:33:53 +0100

    arrow wip

---
+++ SkiaSharpControl/SkiaSharpControl/Draw/Scene/Arrow.cs
@@ -0,0 +1,18 @@
+﻿using SkiaSharp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace SkiaSharpControl.Draw.Scene
+{
+    public class Arrow:BaseShape
+    {
+        public SKColor Color { get; protected set; }
+        public Arrow(Point direction, SKColor color)
+        {
+            Color = Color;
+            //Todo paint and make a stroke base shape that has default width
+        }
+    }
+}
